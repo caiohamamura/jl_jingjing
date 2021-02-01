dcbdbba6589d933846e6bd9cb6503769fbd2d305
Caio Hamamura <caiohamamura@gmail.com>
2021-02-01 18:53:51 -0300

    Print progress messages

--- 01_create_hdf_struct.jl
+++ 01_create_hdf_struct.jl
@@ -43,50 +43,66 @@ function main(args)
         
     
     list_h5 = glob("*02_B*.h5",args[1])
+    total_counter = 0
+
+    n_files = size(list_h5)[1]
+    n_total = 8 * n_files * 5
+    file_count = 0
     for h5_file_path in list_h5
-    in_h5 = h5open(args[1], "r")
-    # HDF5.delete_object(ds_pavd)
-    groups = keys(in_h5)
-    groups = groups[startswith.(groups, "BEAM")]
+        file_count += 1
+        @printf("Processing %s (%d of %d)...\n", h5_file_path, file_count, n_files)
+        in_h5 = h5open(args[1], "r")
+        # HDF5.delete_object(ds_pavd)
+        groups = keys(in_h5)
+        groups = groups[startswith.(groups, "BEAM")]
 
 
-    #group = groups[1]
-    for group in groups
-        g = in_h5[group]
-        n = HDF5.size(g["shot_number"])[1]
-        
-        mask = g["l2b_quality_flag"][:] .== 1
-        seq = [1:1:n;]
-        seq_mask = seq[mask]
-        seq_max = maximum(seq_mask)
-        seq_min = minimum(seq_mask)
-        slice = seq_min:seq_max
-
-        lats = g["geolocation/latitude_bin0"][slice][mask[slice]]
-        y_ind = Int32.(floor.((90 .- lats) ./ y_res))
-        lons = g["geolocation/longitude_bin0"][slice][mask[slice]]
-        x_ind = Int32.(floor.((lons .+ 180) ./ x_res))
-        indices = (y_ind .* xmax .+ x_ind)
-        
-        do
-            ds_pavd = g["pavd_z"]
+        n_groups = size(groups)[1]
+        group_count = 0
+        #group = groups[1]
+        for group in groups
+            group_count += 1
+            @printf("...processing %s (%d of %d)\n", group, group_count, n_groups)
+            g = in_h5[group]
+            n = HDF5.size(g["shot_number"])[1]
             
-            pavd_agg = from_float32.(slice_get.((ds_pavd,), indices))
-            update_stats.(pavd_agg, pavd)
-            slice_set.((ds_pavd,), indices, to_float32.(pavd_agg))
-        end #do
+            mask = g["l2b_quality_flag"][:] .== 1
+            seq = [1:1:n;]
+            seq_mask = seq[mask]
+            seq_max = maximum(seq_mask)
+            seq_min = minimum(seq_mask)
+            slice = seq_min:seq_max
 
-        #col = "pavd"
-        for col in ["rh100", "pai", "cover", "fhd_normal", "pavd"]
-            this_data = col == "pavd" ? 
-                pavd = dropdims(sum(g["pavd_z"][:,slice][:,mask[slice]], dims=1); dims=1) : 
-                g[col][slice][mask[slice]]
-            this_agg = from_float32.(slice_get.((out_ds[col],), indices))
-            update_stats.(this_agg, this_data)
-            slice_set.((out_ds[col],), indices, to_float32.(this_agg))
-        end #cols
+            lats = g["geolocation/latitude_bin0"][slice][mask[slice]]
+            y_ind = Int32.(floor.((90 .- lats) ./ y_res))
+            lons = g["geolocation/longitude_bin0"][slice][mask[slice]]
+            x_ind = Int32.(floor.((lons .+ 180) ./ x_res))
+            indices = (y_ind .* xmax .+ x_ind)
+            
+            do
+                ds_pavd = g["pavd_z"]
+                
+                pavd_agg = from_float32.(slice_get.((ds_pavd,), indices))
+                update_stats.(pavd_agg, pavd)
+                slice_set.((ds_pavd,), indices, to_float32.(pavd_agg))
+            end #do
 
-    end #groups
+            #col = "pavd"
+            n_cols = size(cols)[1]
+            
+            for col in ["rh100", "pai", "cover", "fhd_normal", "pavd"]
+                total_counter += 1
+                @printf("\rProgress...%.2f%%", 100.0*total_counter/n_total)
+                this_data = col == "pavd" ? 
+                    pavd = dropdims(sum(g["pavd_z"][:,slice][:,mask[slice]], dims=1); dims=1) : 
+                    g[col][slice][mask[slice]]
+                this_agg = from_float32.(slice_get.((out_ds[col],), indices))
+                update_stats.(this_agg, this_data)
+                slice_set.((out_ds[col],), indices, to_float32.(this_agg))
+            end #cols
+            println()
 
+        end #groups
+    end #files
     
 end
